132b5732c5760cd4e1ff8789d1ace7378579a4fa
Deepak Wankhede <118713617+deepakwankhede@users.noreply.github.com>
2024-01-26 12:23:42 +0530

    assignment 2.py

--- assignment 2.py
+++ assignment 2.py
@@ -1,8 +1,10 @@
-num = int(input())
+num = int(input("Enter the No Of Test Cases:- "))
 for i in range(num):
    count = 0
-   num,k = list(map(int,input().split()))
-   heights = list(map(int,input().split()))
+   """num,k = list(map(int,input().split()))"""
+   num = int(input("Total number of players between them:- "))
+   k = int(input("Height of Ali & Gi-Hun:- "))
+   heights = list(map(int,input(f"Enter the {num}'s players heights:- ").split()))
    for i in heights:
        if i > k:
            count+=1
